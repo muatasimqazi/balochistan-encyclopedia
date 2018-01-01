b8c3d6c4cfef7bf73e1bf9ab6fed9acb80de2318
Muatasim Qazi <muatasimqazi@gmail.com>
2018-01-01 03:49:22 -0800

    added footer content

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -4,24 +4,33 @@ import { withStyles } from 'material-ui/styles';
 import Paper from 'material-ui/Paper';
 import Grid from 'material-ui/Grid';
 import Typography from 'material-ui/Typography'
-import List, { ListItem, ListItemText } from 'material-ui/List';
+import List, { ListItem, ListItemIcon, ListItemText } from 'material-ui/List';
+import {FaFacebook, FaTwitter}  from 'react-icons/lib/fa';
+
 
 const styles = theme => ({
     root: {
-      padding: 16,
       marginTop: theme.spacing.unit * 3,
       background: '#227AAC',
       color: '#ffff'
     },
-    paper: {
-        background: '#227AAC',
-        color: '#ffff',
-        margin: 30,
+    text: {
+        fontWeight: 'normal !important',
     },
-    list: {
-        color: '#ffff',
-    }
-
+    item: {
+        marginRight: 70,
+        marginBottom: 40,
+    },
+    copyright: {
+        background: '#075a8a',
+    },
+   
+    gutters: {
+        paddingLeft: 0,
+    },
+    icon: {
+        marginRight: 0
+    },
   });
 
 class Footer extends Component {
@@ -34,56 +43,54 @@ class Footer extends Component {
     render() {
         const { classes } = this.props;
         return (
-
             <footer>
-                <div className={classes.root}>
-            <Grid container spacing={24}>
-            <Grid item xs={12} sm={8}>
-            <Paper className={classes.paper} elevation={0}>
-            <Typography type="title" className={classes.title}>
-              Text only
-            </Typography>
-            <div>
-              <List>
-                <ListItem button>
-                <ListItemText
-                    primary="Single-line item"
-                />
-                </ListItem>
-                <ListItem button>
-                <ListItemText
-                    primary="Single-line item"
-                />
-                </ListItem>
-                <ListItem button>
-                <ListItemText
-                    primary="Single-line item"
-                />
-                </ListItem>
-                <ListItem button>
-                <ListItemText
-                    primary="Single-line item"
-                />
-                </ListItem>
-              </List>
-            </div>
-            </Paper>
-          </Grid>
-          <Grid item xs={12} sm={4}>
-            <Paper className={classes.paper} elevation={0}>xs=12 sm=6</Paper>
-          </Grid>
-          
-            <Grid item xs={12}>
-                <div className="copyright" style={{textAlign: 'right'}}>
-                        &copy; {this.state.year}, Designed and Coded by
-                        <a href="" target="_blank"> Q. Creatives</a>.
-                </div>
-            </Grid>
-            </Grid>
-            
+            <div style={{ padding: 20 }}>
+                <Grid container className={classes.root} spacing={40}>
+                    <Grid item xs={12} sm={12} md={4} className={classes.item}>
+                        <Typography type="title" gutterBottom>
+                            Balochistan Encyclopedia
+                        </Typography>
+                        <Typography type="body2" gutterBottom className={classes.text}>
+                            Balochistan Encyclopedia is a crowd-sourced online encyclopedia that aims to become an extensive resource on all things Balochistan.
+                        </Typography>
+                    </Grid>
+
+                    <Grid item xs={12} sm={12} md={3} className={classes.item}>
+                        <Typography type="title" gutterBottom>
+                                Follow Us
+                        </Typography>
+                        <List>
+                            <ListItem button component="a" href="#facebook" className={classes.gutters}>
+                            <ListItemIcon className={classes.icon}>
+                                <FaFacebook size={30} />
+                            </ListItemIcon>
+                            <ListItemText inset={true} primary="Facebook" />
+                            </ListItem>
+
+                            <ListItem button component="a" href="#twitter" className={classes.gutters}>
+                            <ListItemIcon className={classes.icon}>
+                                <FaTwitter size={30} />
+                            </ListItemIcon>
+                            <ListItemText primary="Twitter" />
+                            </ListItem>
+                        </List>
+                        
+                    </Grid>
+                    <Grid item xs={12} sm={12} md={3} className={classes.item}>
+                        <Typography type="title" gutterBottom>
+                        Contribution Policy
+                        </Typography>
+                    </Grid>
+                    
+                    <Grid item xs={12} sm={12} md={12} className={classes.copyright}>
+                        <Typography type="caption" gutterBottom align="right">
+                        &copy; {this.state.year}, Balochistan Encyclopdiea | Designed and Coded by
+                                <a href="" target="_blank"> Q. Creatives</a>
+                        </Typography>
+                    </Grid>
+                </Grid>
             </div>
             </footer>
-
         );
     }
 }
